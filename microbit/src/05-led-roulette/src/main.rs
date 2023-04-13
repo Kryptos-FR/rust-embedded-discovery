7d3be59fb5d3e1e69e6990d5a1d80b4f28faa108
Nicolas Musset <musset.nicolas@gmail.com>
2023-04-13 14:13:29 +0200

    5. My solution

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,13 +3,9 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 use panic_rtt_target as _;
-use microbit::{
-    board::Board,
-    display::blocking::Display,
-    hal::{prelude::*, Timer},
-};
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
@@ -18,19 +14,26 @@ fn main() -> ! {
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-    let light_it_all = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
 
+    let mut last_led = (0, 0);
+
+    let series = [0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 4, 3, 2, 1];
+    let shift = 4;
+
     loop {
-        // Show light_it_all for 1000ms
-        display.show(&mut timer, light_it_all, 1000);
-        // clear the display again
-        display.clear();
-        timer.delay_ms(1000_u32);
+        for i in 0..series.len() {
+            let current_led = &(series[i], series[(i + shift) % series.len()]);
+            leds[last_led.0][last_led.1] = 0;
+            leds[current_led.0][current_led.1] = 1;
+            display.show(&mut timer, leds, 62);
+            last_led = *current_led;
+        }
     }
 }
