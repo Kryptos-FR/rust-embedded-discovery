b51f34272853bc06c6508ca6113fd5c0016adb92
Nicolas Musset <musset.nicolas@gmail.com>
2023-04-13 11:56:49 +0200

    5. Delaying

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,17 +3,21 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
 use microbit::board::Board;
 use microbit::hal::prelude::*;
+use microbit::hal::timer::Timer;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        timer.delay_ms(1000u16);
+        rprintln!("1000 ms passed");
+    }
 }
